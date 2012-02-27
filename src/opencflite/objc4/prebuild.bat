8c50bb8b5526530d38bb11c53cf44f9be9191396
Leonardo Carneiro <leonardo@unity3d.com>
2012-02-27 15:30:00 +0100

    Fix building debug in VS2005. Special issues with objc. Found help in: https://github.com/Ashod/WinCairoRequirements/commits/

--- src/opencflite/objc4/prebuild.bat
+++ src/opencflite/objc4/prebuild.bat
@@ -1,6 +1,7 @@
 @echo off
 
 echo prebuild: installing headers
+call version.bat
 xcopy /Y "%ProjectDir%runtime\objc.h" "%SolutionDir%include\objc\"
 xcopy /Y "%ProjectDir%runtime\objc-api.h" "%SolutionDir%include\objc\"
 xcopy /Y "%ProjectDir%runtime\objc-auto.h" "%SolutionDir%include\objc\"
